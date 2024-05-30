6791eef9c992d0e25b7d22c29a076f02054186cc
Guro Bokum <jiojiajiu@gmail.com>
2024-05-30 20:07:01 +0300

    feat(nextjs): add mobile viewports and i18n to storybook

--- packages/nextjs/.storybook/main.ts
+++ packages/nextjs/.storybook/main.ts
@@ -9,12 +9,28 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-styling-webpack",
-    "@storybook/addon-themes"
+    "@storybook/addon-themes",
+    "@storybook/addon-viewport",
+    "storybook-react-i18next",
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
+  webpackFinal: async config => {
+    if (!config.module) {
+      return config;
+    }
+    if (!config.module.rules) {
+      config.module.rules = [];
+    }
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: "yaml-loader",
+    });
+    return config;
+  },
+
   staticDirs: ["../public"],
 };
 export default config;
--- packages/nextjs/.storybook/preview.ts
+++ packages/nextjs/.storybook/preview.ts
@@ -2,17 +2,29 @@ import type { Preview } from "@storybook/react";
 import "../src/app/globals.css";
 
 import { withThemeByDataAttribute } from "@storybook/addon-themes";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+import i18n from "../src/i18n";
 
 const preview: Preview = {
+  globals: {
+    locale: "en",
+    locales: {
+      en: "English",
+      ru: "Русский",
+    },
+  },
   parameters: {
+    i18n,
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    viewport: {
+      viewports: MINIMAL_VIEWPORTS,
+    },
   },
-
   decorators: [
     withThemeByDataAttribute({
       themes: {
