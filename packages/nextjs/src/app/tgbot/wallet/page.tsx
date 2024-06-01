be7b8909d5d67d65e397f3a09abe47b85c76ec4d
Guro Bokum <jiojiajiu@gmail.com>
2024-06-01 17:45:13 +0300

    feat(nextjs): add wallet page

---
+++ packages/nextjs/src/app/tgbot/wallet/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { TonConnectButton } from "@tonconnect/ui-react";
+
+export default function WalletPage() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <TonConnectButton />
+    </div>
+  );
+}
